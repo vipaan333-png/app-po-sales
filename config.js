1a21dc0b352bcfae0c054e98c599216bfb3eb216
vipaan333-png <vip.aan333@gmail.com>
2026-01-28 11:22:41 +0700

    Update SCRIPT_URL to personal spreadsheet backend

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // Google Apps Script Configuration
 const CONFIG = {
     // Google Apps Script Web App URL
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzyblL4XFvWWTeRxiy9wu0gSXjbwaZnSmfwtkBjfU_8YxtvOSlo60DzdxjwYyLYALTR/exec',
-    
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyA6kLQVxkVx1_Vl5HXOKoJK9qoKIZrgU_tp8N8hAqeCR_QLmTgAme7wMmYXQk-HPKYAQ/exec',
+
     // Application Settings
     APP_NAME: 'APP PO SALES',
     PO_PREFIX: 'PO',
-    
+
     // Payment Methods
     PAYMENT_METHODS: ['KREDIT', 'CASH', 'COD', 'DP']
 };
